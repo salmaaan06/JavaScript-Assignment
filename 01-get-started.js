795886542de1142c901a7800eb1b3224ebb8c6ad
salmaaan06 <126793117+salmaaan06@users.noreply.github.com>
2026-04-28 22:37:25 +0530

    Add files via upload

---
+++ 01-get-started.js
@@ -0,0 +1,39 @@
+/* console.log("step 1")
+setTimeout(() => console.log("step 2"));
+console.log("Step 3") */
+
+//number
+
+var phoneNumber = 9999999
+console.log(phoneNumber)
+console.log(typeof(phoneNumber))
+
+//string
+
+var fullName = "Salman"
+/* var middlename = 'Das'
+var lastName = `S` */
+
+console.log(fullName)
+console.log(typeof(fullName))
+
+//boolean
+
+var validUser = true
+console.log(validUser)
+console.log(typeof(validuser))
+
+//null
+
+var accountNumber = null
+console.log(accountNumber)
+console.log(typeof(accountNumber))
+
+//undfined
+
+var middleName
+console.log(middleName)
+console.log(typeof(middleName))
+
+
+
