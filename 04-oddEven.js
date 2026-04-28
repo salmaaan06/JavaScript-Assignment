7a1ee0557041e27a32c54d6df638a420b35c7100
salmaaan06 <126793117+salmaaan06@users.noreply.github.com>
2026-04-28 22:54:40 +0530

    Add files via upload

---
+++ 04-oddEven.js
@@ -0,0 +1,10 @@
+function oddOrEvenNumbers(){
+    for(i=0; i<=20; i++){
+        if(i%2 === 0){
+            console.log(i,"-Even number")
+        }else{
+            console.log(i, "-Odd number")
+        }
+    }
+}
+oddOrEvenNumbers()
